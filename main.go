ca94cfe1bfcd921552364325b88d4fbaec0a87aa
Muhammed Faris <farismuhammed707@gmail.com>
2024-11-21 16:04:37 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/faris-muhammed/hotel-booking-platform/user-service/internal/handler"
+	"github.com/faris-muhammed/hotel-booking-platform/user-service/internal/repository"
+	"github.com/faris-muhammed/hotel-booking-platform/user-service/internal/service"
+	pb "github.com/faris-muhammed/hotel-booking-platform/user-service/proto"
+	hotel_pb "github.com/faris-muhammed/hotel-booking-platform/user-service/proto/client/hotel"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+
+	connHotel, err := grpc.Dial("localhost:8081", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect to user service: %v", err)
+	}
+	defer connHotel.Close()
+	hotelService := hotel_pb.NewHotelServiceClient(connHotel)
+
+	repo := repository.NewUserRepo()
+	userService := service.NewUserService(repo, hotelService)
+	userHandler := handler.NewUserHandler(userService)
+
+	listen, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal("failed to listen, err: ", err)
+	}
+
+	g := grpc.NewServer()
+	pb.RegisterUserServiceServer(g, userHandler)
+
+	reflection.Register(g)
+
+	log.Println("Server listening on port :8080")
+	if err := g.Serve(listen); err != nil {
+		log.Fatal("failed to listen - err: ", err)
+	}
+
+}
